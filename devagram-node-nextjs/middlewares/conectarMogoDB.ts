afd4e9a661954256911931794abf391c14e876e5
Walace Dias Calmon <walace.calmon@gmail.com>
2022-06-28 14:12:33 -0300

    Configuração de banco de dados

---
+++ devagram-node-nextjs/middlewares/conectarMogoDB.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import mongoose from 'mongoose';
+import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
+
+export const conectarMongoDB = (handler :  NextApiHandler) => 
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    
+    // Verificar se o banco ja esta conectado, se estiver seguir para o endpoint ou proximo middleware
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
+    }
+
+    // Se não estiver conectado vamos conectar
+    // Obter a variavel de ambiente preenchida do .env.local
+    const {DB_COXEXAO_STRING} = process.env;
+    
+    // Se a .env.local estiver vazia aborta o uso do sistema e informa o programador
+    if(!DB_COXEXAO_STRING){
+        return res.status(500).json({ erro : 'ENV de configuração do banco, não informada.'})
+    }
+
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', erro => console.log(`Ocorrou erro ao conectar ao banco ${erro}`));
+    await mongoose.connect(DB_COXEXAO_STRING);
+
+    // Agora posso seguir para o endpoint, pois agora estou conectado ao banco
+    return handler(req, res);
+}
---
+++ devagram-node-nextjs/pages/api/login.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import {conectarMongoDB} from '../../middlewares/conectarMogoDB';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const endpointLogin = (
+    req : NextApiRequest,
+    res : NextApiResponse<RespostaPadraoMsg>
+) => {
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
+        if(login === 'admin@admin.com' && senha === 'Admin@123'){
+            return res.status(200).json({msg : 'Usuário autenticado com sucesso!'});
+        }
+        return res.status(400).json({erro : 'Usuário ou senha inválido!'});
+    }
+    return res.status(405).json({erro : 'Metodo não é válido'});
+}
+
+export default conectarMongoDB(endpointLogin);
